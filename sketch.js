0a667e115c28541f6d5d364746e6014520ff9711
amischreibman <130557712+amischreibman@users.noreply.github.com>
2025-04-06 09:39:41 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let easeOutPower = 10;
 let textShadowBlur = 0;                    
 let textShadowColor = 'white';             
 let textMaxSizePercentage = 0.6;           
-let textContentPadding = 100;               // מרווח בין כותרת העיגול לתוכן הטקסט
+let textContentPadding = 200;               // מרווח בין כותרת העיגול לתוכן הטקסט
 let textFadeInDelay = 300;                 // דיליי במילישניות לפני תחילת הפייד אין של הטקסט
 let textFadeInSpeed = 0.5;                 // מהירות הפייד אין של הטקסט (ערך גבוה = מהיר יותר)
 
